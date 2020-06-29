1737cd01e7a33ec7cc332f318d1f0e83d12f56d9
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-29 14:58:48 -0500

    Updated code to show the neighborhoods of toronto

--- static/Js/logic.js
+++ static/Js/logic.js
@@ -1,13 +1,13 @@
 
 // Create the tile layer that will be a background option of our map
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
 // Create the tile layer that will be a background option of our map
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -16,27 +16,27 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-	Street: light,
-	Dark: dark
+	Streets: streets,
+	"Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center:[44.0, -80.0],
-	zoom: 2,
-	layers: [light]
+	center:[43.7, -79.3],
+	zoom: 11,
+	layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
  
-// Toronto Data URL
-let torontoDataURL = "https://raw.githubusercontent.com/berndab/mapping_earthquakes/muliple_geojson_lines/torontoRoutes.json";
+// Toronto city neighborhoods GeoJSON URL.
+let torontoNeighborhoodsURL = "https://raw.githubusercontent.com/berndab/mapping_earthquakes/geojson_polygons/torontoNeighborhoods.json";
 
 // GeoJSON data.
-d3.json(torontoDataURL).then(function(data) {
+d3.json(torontoNeighborhoodsURL).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJSON(data).addTo(map);
 });
