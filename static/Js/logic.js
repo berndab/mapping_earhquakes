174e3ff53fdcf6ffe25646a2f9eae56a5b43f14e
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-29 11:57:39 -0500

    Adding updated logic file to handle json and airport jason files

--- static/Js/logic.js
+++ static/Js/logic.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,6 +9,49 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	id: 'mapbox.streets',
 	accessToken: API_KEY
 });
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add San Francisco Airport GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+	"type":"Feature",
+	"properties":{
+		"id":"3469",
+		"name":"San Francisco International Airport",
+		"city":"San Francisco",
+		"country":"United States",
+		"faa":"SFO",
+		"icao":"KSFO",
+		"alt":"13",
+		"tz-offset":"-8",
+		"dst":"A",
+		"tz":"America/Los_Angeles"},
+		"geometry":{
+			"type":"Point",
+			"coordinates":[-122.375,37.61899948120117]}}
+]};
+
+
+// Adding GeoJSON data to the map
+// L.geoJSON(sanFranAirport, {
+// 	// We turn each feature into a marker on the map.
+// 	pointToLayer: function(feature, latlng) {
+// 	console.log(feature);
+// 	return L.marker(latlng)
+// 	.bindPopup("<h2>" + feature.properties.city + "</h2>");
+// 	}
+// }).addTo(map);
+
+// Adding GeoJSON data to the map
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(frature, layer){
+		console.log(layer);
+		layer.bindPopup();
+	}
+}).addTo(map);
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
