5f9085e1702af6f9f9ceeb156a4bed218ed07f14
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-29 14:34:32 -0500

    Added new data for geojson polygons

--- static/Js/logic.js
+++ static/Js/logic.js
@@ -1,9 +1,8 @@
 
 // Create the tile layer that will be a background option of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
-	id: 'mapbox.streets',
 	accessToken: API_KEY
 });
 
@@ -17,33 +16,26 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-	Street: streets,
+	Street: light,
 	Dark: dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [30, 30],
+	center:[44.0, -80.0],
 	zoom: 2,
-	layers: [streets]
+	layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
  
-// Airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/berndab/mapping_earthquakes/multiple_geojson_points/majorAirports.json";
+// Toronto Data URL
+let torontoDataURL = "https://raw.githubusercontent.com/berndab/mapping_earthquakes/muliple_geojson_lines/torontoRoutes.json";
 
 // GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
-});
-
-// GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoDataURL).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
