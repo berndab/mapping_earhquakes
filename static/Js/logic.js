c7a83d09d68e51ceb6967f10f8718dd1c5462b03
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-29 12:30:37 -0500

    Added code to display multiple geojson points

--- static/Js/logic.js
+++ static/Js/logic.js
@@ -10,48 +10,20 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 	accessToken: API_KEY
 });
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
-//Add San Francisco Airport GeoJSON data.
-let airportData =
-{
-	"type":"FeatureCollection","features":[{
-	"type":"Feature",
-	"properties":{
-		"id":"3469",
-		"name":"San Francisco International Airport",
-		"city":"San Francisco",
-		"country":"United States",
-		"faa":"SFO",
-		"icao":"KSFO",
-		"alt":"13",
-		"tz-offset":"-8",
-		"dst":"A",
-		"tz":"America/Los_Angeles"},
-		"geometry":{
-			"type":"Point",
-			"coordinates":[-122.375,37.61899948120117]}}
-]};
+// Accessing the airport GeoJSON URL
+ let airportData = "https://raw.githubusercontent.com/berndab/mapping_earthquakes/multiple_geojson_points/majorAirports.json";
 
 
-// Adding GeoJSON data to the map using pointToLayer
-// L.geoJSON(airportData, {
-// 	// We turn each feature into a marker on the map.
-// 	pointToLayer: function(feature, latlng) {
-// 	console.log(feature);
-// 	return L.marker(latlng)
-// 	.bindPopup("<h2>" + feature.properties.city + "</h2>");
-// 	}
-// }).addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
 
-// Adding GeoJSON data to the map using onEachFeature
-L.geoJSON(airportData, {
-    onEachFeature: function(frature, layer){
-		console.log(layer);
-		layer.bindPopup();
-	}
-}).addTo(map);
 
 
 // Then we add our 'graymap' tile layer to the map.
