b45321af3ac9e36e695447c91287c95bafa628aa
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-29 18:20:02 -0500

    Adding popup markers for earthquakes

--- static/Js/logic copy 2.js
+++ static/Js/logic copy 2.js
@@ -46,39 +46,19 @@ d3.json(geoJsonDataURL).then(function(data) {
 		return magnitude * 4;
 	};
 
-	// This function determines the color of the circle based on the magnitude of the earthquake.
-	function getColor(magnitude) {
-		if (magnitude > 5) {
-			return "#ea2c2c";
-		}
-		if (magnitude > 4) {
-			return "#ea822c";
-		}
-		if (magnitude > 3) {
-			return "#ee9c00";
-		}
-		if (magnitude > 2) {
-			return "#eecc00";
-		}
-		if (magnitude > 1) {
-			return "#d4ee00";
-		}
-		return "#98ee00";
-	}
-
 	// This function returns the style data for each of the earthquakes we plot on
-	// the map. We pass the magnitude of the earthquake into two separate functions
-	// to calculate the color and radius.
+	// the map. We pass the magnitude of the earthquake into a function
+	// to calculate the radius.
 	function styleInfo(feature) {
 		return {
 			opacity: 1,
 			fillOpacity: 1,
-			fillColor: getColor(feature.properties.mag),
+			fillColor: "#ffae42",
 			color: "#000000",
 			radius: getRadius(feature.properties.mag),
 			stroke: true,
 			weight: 0.5
-		};
+		}
 	};
 
 	// Creating a GeoJSON layer with the retrieved data.
@@ -88,12 +68,7 @@ d3.json(geoJsonDataURL).then(function(data) {
 					console.log(latlng);
 					return L.circleMarker(latlng);
 				},
-		style: styleInfo,
-		// Create a popup for each circleMarker to display the magnitude and
-		// location of the earthquake after the marker has been created and styled.
-		onEachFeature: function(feature, layer) {
-			layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-	  	}
+		style: styleInfo
 	}).addTo(map);
 
 });
